0a20488b7b03b133ad8baa2410f122da21e6841e
PSL2001 <pablitosanchezlopez@gmail.com>
2023-04-13 11:29:21 +0200

    Creado metodo de enviar mensaje

--- src/main/java/com/viewnext/WatsonAssistantService.java
+++ src/main/java/com/viewnext/WatsonAssistantService.java
@@ -4,10 +4,16 @@ import javax.websocket.Session;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.cloud.sdk.core.security.Authenticator;
 import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.assistant.v2.model.MessageInput;
+import com.ibm.watson.assistant.v2.model.MessageOptions;
 import com.ibm.watson.assistant.v2.Assistant;
 import com.ibm.watson.assistant.v2.model.CreateSessionOptions;
+import com.ibm.watson.assistant.v2.model.MessageResponse;
 import com.ibm.watson.assistant.v2.model.SessionResponse;
 
 @Service
@@ -34,6 +40,23 @@ public class WatsonAssistantService {
             return service;
         } catch (Error e) {
             throw new Error("Error al conectar con el servicio de Watson Assistant" + e.getMessage());
+        }
+    }
+
+    WatsonAssistantMessage sendMessage(String message) {
+        try {
+            if (this.assistant == null) {
+                this.assistant = connect();
+                this.session = createSession(assistant);
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println(session.getSessionId());
+            MessageInput input = new MessageInput.Builder().text(message).build();
+            MessageOptions messageOptions = new MessageOptions.Builder().assistantId(config.getId()).sessionId(session.getSessionId()).input(input).build();
+            MessageResponse messageResponse = assistant.message(messageOptions).execute().getResult();
+            return mapper.readValue(messageResponse.toString(), WatsonAssistantMessage.class);
+        } catch (JsonProcessingException e) {
+            throw new Error("Error al mapear el mensaje de respuesta de Watson Assistant" + e.getMessage());
         }
     }
 }
