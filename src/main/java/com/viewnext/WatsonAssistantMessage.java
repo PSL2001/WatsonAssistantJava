0615321400f2c12fee46ac22c33c2e79912b804f
PSL2001 <pablitosanchezlopez@gmail.com>
2023-04-13 11:08:44 +0200

    Añadido mensajeria

---
+++ src/main/java/com/viewnext/WatsonAssistantMessage.java
@@ -0,0 +1,46 @@
+package com.viewnext;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class WatsonAssistantMessage {
+    private JsonNode output;
+    private JsonNode generic;
+    private JsonNode intents;
+    private JsonNode entities;
+    private String sessionId;
+
+    //Getters
+    public JsonNode getOutput() {
+        return output;
+    }
+    public JsonNode getGeneric() {
+        return generic;
+    }
+    public JsonNode getIntents() {
+        return intents;
+    }
+    public JsonNode getEntities() {
+        return entities;
+    }
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    //Setters
+    public void setOutput(JsonNode output) {
+        this.output = output;
+    }
+    public void setGeneric(JsonNode generic) {
+        this.generic = generic;
+    }
+    public void setIntents(JsonNode intents) {
+        this.intents = intents;
+    }
+    public void setEntities(JsonNode entities) {
+        this.entities = entities;
+    }
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+}
