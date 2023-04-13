3e39d1352e9115a833ff91edcb3c7cbe5630f960
PSL2001 <pablitosanchezlopez@gmail.com>
2023-04-13 11:55:22 +0200

    Añadido funcion para enviar mensaje (controlador)

--- src/main/java/com/viewnext/WatsonAssistantController.java
+++ src/main/java/com/viewnext/WatsonAssistantController.java
@@ -1,11 +1,29 @@
 package com.viewnext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 public class WatsonAssistantController {
-    @RequestMapping(value = "/send", method = RequestMethod.GET)
-    public String send(@RequestParam("message") String message) {
-        return message;
+
+    private WatsonAssistantService watsonAssistantService;
+
+    @Autowired
+    public WatsonAssistantController(WatsonAssistantService watsonAssistantService) {
+        this.watsonAssistantService = watsonAssistantService;
+    }
+    
+    @RequestMapping(value="/send", method = RequestMethod.GET)
+    public JsonNode send(@RequestParam("message") String message) throws JsonProcessingException {
+
+        //String response1 = watsonAssistantService.sendMessage("Hola");
+        WatsonAssistantMessage response;
+        response = watsonAssistantService.sendMessage(message);
+
+        return response.getGeneric();
+
     }
 }
