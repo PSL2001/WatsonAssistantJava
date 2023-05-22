29375380c090d54a4c3c0316e724bde450dcdc22
PSL2001 <pablitosanchezlopez@gmail.com>
2023-05-22 11:57:29 +0200

    Añadida documentacion base

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.watson.app',
+  appName: 'Watson Assistant Bot',
+  webDir: 'dist/frontend',
+  bundledWebRuntime: false
+};
+
+export default config;
