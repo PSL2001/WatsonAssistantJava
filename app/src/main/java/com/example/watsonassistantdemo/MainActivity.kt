9efe6af4d7db2ef51fe496a1deb8597a5c5dddf6
PSL2001 <pablitosanchezlopez@gmail.com>
2023-06-07 12:17:17 +0200

    Añadido comentarios

--- app/src/main/java/com/example/watsonassistantdemo/MainActivity.kt
+++ app/src/main/java/com/example/watsonassistantdemo/MainActivity.kt
@@ -10,31 +10,66 @@ import android.webkit.WebViewClient
 import com.example.watsonassistantdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * Binding para la vista principal
+     * @see ActivityMainBinding (Android)
+     * @see AppCompatActivity (Android)
+     * @see onCreate (Android)
+     */
     lateinit var binding: ActivityMainBinding
 
+    /**
+     * Método que se ejecuta al iniciar la aplicación
+     * @see onCreate (Android)
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Se crea la vista
         super.onCreate(savedInstanceState)
+        // Se asigna la vista al binding
         binding = ActivityMainBinding.inflate(layoutInflater)
+        // Se asigna la vista al activity usando el binding
         setContentView(binding.root)
+        // Se ejecuta el método para configurar el WebView
         webViewSetup()
     }
 
+    /**
+     * Método para configurar el WebView
+     * @see webViewSetup (Android)
+     * @see WebView (Android)
+     * @see WebViewClient (Android)
+     *
+     */
     private fun webViewSetup() {
+        // Se asigna el WebViewClient al WebView
         binding.webView.webViewClient = MyWebViewClient()
+        // Se aplican las configuraciones al WebView
         binding.webView.apply {
+            // Se habilita el modo de depuración, para poder ver los errores en la consola Logcat
             WebView.setWebContentsDebuggingEnabled(true)
+            // Se carga la URL del proyecto de GitHub
             try {
                 loadUrl("https://psl2001.github.io/WatsonAssistantJava/")
             } catch (e: Exception) {
+                // Si ocurre un error, se imprime en la consola Logcat
                 e.printStackTrace()
             }
+            // Se habilita el JavaScript, necesario para que funcione el chatbot de Watson
             settings.javaScriptEnabled = true
-            settings.allowFileAccess = true
+            // Se habilita safe browsing, para que no se puedan abrir páginas maliciosas. No es necesario, pero es recomendable
             settings.safeBrowsingEnabled = true
+            // Se habilita el modo de mezcla de contenido, para que se puedan cargar páginas HTTP en páginas HTTPS.
+            // Aunque la pagina del chabot es HTTPS, es posible que en otros casos se necesite cargar por HTTP
             binding.webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         }
     }
 
+    /**
+     * Método para sobreescribir el botón de retroceso
+     * @see onBackPressed (Android)
+     * @see WebView (Android)
+     * @see Deprecated (Deprecated in Java)
+     */
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         if (binding.webView.canGoBack()) {
@@ -44,28 +79,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * Clase para sobreescribir el WebViewClient
+     * @see MyWebViewClient (Android)
+     * @see WebViewClient (Android)
+     */
     private inner class MyWebViewClient : WebViewClient() {
+        /**
+         * Método para sobreescribir la carga de una URL
+         * @see shouldOverrideUrlLoading (Android)
+         * @see WebView (Android)
+         * @see WebResourceRequest (Android)
+         */
         override fun shouldOverrideUrlLoading(
             view: WebView?,
             request: WebResourceRequest?
         ): Boolean {
-            return false;
-        }
-
-
-        override fun onPageFinished(view: WebView, url: String) {
-            // Aquí puedes realizar acciones después de que la página se haya cargado completamente
-            // Por ejemplo, mostrar un mensaje o realizar alguna operación en función de la URL cargada
-            try {
-                if (url == "file:///android_asset/index.html") {
-                    println("Página cargada")
-                    // La página index.html ha cargado completamente
-                    // Realiza las acciones necesarias aquí
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-            super.onPageFinished(view, url)
+            // Retorna falso para que se cargue la URL
+            return false
         }
     }
 
